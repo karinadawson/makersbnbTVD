25be5addd46ab0fa1051ccff6f26642054efec07
Maz <mazenmagzoub@gmail.com>
2024-07-11 20:44:50 +0100

    maz local changes before pulling

--- client/src/components/PaperComp.jsx
+++ client/src/components/PaperComp.jsx
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 
 const PaperComp = () => {
-const [spacesState, setSpacesState] = useState([])
+const [spacesState, setSpacesState] = useState({})
 
 const fetchSpaces = async () => {
     const response = await axios.get('http://127.0.0.1:5002/spaces');
